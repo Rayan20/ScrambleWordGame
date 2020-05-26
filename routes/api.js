bf26262ff6062ddbf381268bdebffb2a184381b0
Yang, Richard <Richard.Yang@pearson.com>
2020-05-25 23:27:54 -0500

    edited css and re-formatted website

--- routes/api.js
+++ routes/api.js
@@ -8,5 +8,9 @@ router.get('/random', function(req, res, next) {
 router.get('/bonus', function(req, res, next) {
     res.render('bonus', {page:'', menuId:'bonus'});
 });
+router.get('/signup', function(req, res, next) {
+    res.render('signup', {page:'', menuId:'signup'});
+});
+
 
 module.exports = router;

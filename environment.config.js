773ca7df9c448f3a7517a99dfa91c0dc1f9a7fd1
Yang, Richard <Richard.Yang@pearson.com>
2020-06-02 13:59:22 -0500

    Updated Normal and Bonus Mode pages and fixed Sign out button

--- environment.config.js
+++ environment.config.js
@@ -10,11 +10,11 @@ module.exports = {
         enableCluster:false
     },
     db: {
-        host: "localhost",
-        user: "root",
-        password: "password",
+        host: "ec2-52-72-65-76.compute-1.amazonaws.com",
+        user: "iovguoobcsamqj",
+        password: "b91f65194a2466bd71c5d77410e43b3d0e4c7ffde6b259af99cc35c7f2345e8d",
         port: "5432",
-        database: "web",
+        database: "db6ia04fg21gv9",
         debug: false,
         //multipleStatements: true,
         //connectionLimit: 10,

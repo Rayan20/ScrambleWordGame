7397c39d80c17ad7195a3963ed524d4d02a68871
Rayan20 <47995393+Rayan20@users.noreply.github.com>
2020-06-02 13:45:04 -0500

    Update environment.config.js
    changed file

--- environment.config.js
+++ environment.config.js
@@ -10,7 +10,7 @@ module.exports = {
         enableCluster:false
     },
     db: {
-        host: "ec2-35-169-254-43.compute-1.amazonaws.com",
+        host: "ec2-52-72-65-76.compute-1.amazonaws.com",
         user: "iovguoobcsamqj",
         password: "b91f65194a2466bd71c5d77410e43b3d0e4c7ffde6b259af99cc35c7f2345e8d",
         port: "5432",

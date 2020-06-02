10ce090a4cf1f6363baa210b43bcd13293203f53
Yang, Richard <Richard.Yang@pearson.com>
2020-06-02 13:56:50 -0500

    fixed api bug

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,21 @@ const loginController = require('../controller/loginController');
 /* GET home page. */
 router.get('/random', function(req, res, next) {
     var mycookie = req.cookies[ScrambeWordGameCookie];
-    res.render('random', {page:'', menuId:'random',Username:mycookie});
+    if(!mycookie){
+        res.render('login', {loginMessage:''});
+    }
+    else{
+        res.render('index', {page:'', menuId:'random',Username:mycookie});
+    }
 });
 router.get('/bonus', function(req, res, next) {
     var mycookie = req.cookies[ScrambeWordGameCookie];
-    res.render('bonus', {page:'', menuId:'bonus',Username:mycookie});
+    if(!mycookie){
+        res.render('login', {loginMessage:''});
+    }
+    else{
+        res.render('index', {page:'', menuId:'random',Username:mycookie});
+    }
 });
 router.get('/clear_cookie', function(req, res, next) {
     res.clearCookie(ScrambeWordGameCookie);

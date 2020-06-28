7213e254bf6e86853039c597b8584482ff664b4d
Yang, Richard <Richard.Yang@pearson.com>
2020-06-28 17:35:28 -0500

    update heroku setting

--- environment.config-local.js
+++ environment.config-local.js
@@ -10,11 +10,11 @@ module.exports = {
         enableCluster:false
     },
     db: {
-        host: "ec2-52-72-65-76.compute-1.amazonaws.com",
-        user: "iovguoobcsamqj",
-        password: "b91f65194a2466bd71c5d77410e43b3d0e4c7ffde6b259af99cc35c7f2345e8d",
+        host: "localhost",
+        user: "root",
+        password: "password",
         port: "5432",
-        database: "db6ia04fg21gv9",
+        database: "web",
         debug: false,
         //multipleStatements: true,
         //connectionLimit: 10,
